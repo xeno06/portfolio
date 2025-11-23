938418ec5c2c2433898f70c00f0695e867d2d821
xenoxenoxeno0 <vincequijano159@gmail.com>
2025-11-24 02:03:07 +0800

    uploaded

--- myport/src/App.js
+++ myport/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './styles/App.css';
 
 function App() {
   return (
-    <Router>
+    <div className="app">
       <Navbar />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </main>
-      <Footer />
-    </Router>
+      <Footer/>
+    </div>
   );
 }
 
